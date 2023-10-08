cd9099f717768cd0cce2cabb5b29c65bd259c9d0
ChcoudhurySneha <ch.sneha31@gmail.com>
2023-10-08 17:58:30 +0530

    javscript

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello World")
+console.log(1+1)
+console.log("1"+"1")
+console.log(1<2)
+alert("Welcome to my app")
+var userAge=prompt("What is your age")
+if(userAge<18){
+    alert("you are not allowed")
+} else {
+    alert("you are welcome to come to the bar")
+}
